3d3c040b0e73fdefd144f220ac43999939c4088a
ThuPham02 <s3878246@rmit.edu.vn>
2021-12-30 16:28:25 +0700

    add font

--- js/game.js
+++ js/game.js
@@ -118,7 +118,7 @@ choices.forEach(choice => {
         setTimeout(()=>{
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        }, 1700)
+        }, 1500)
     })
 })
 
