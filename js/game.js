ae3a6247360614c0afdce3e2fa50b7c5fb85a3da
ThuPham02 <s3878246@rmit.edu.vn>
2021-12-30 19:53:20 +0700

    fix minor bugs
    - fix font-size
    - add spaces btw letter
    - delete font folder

--- js/game.js
+++ js/game.js
@@ -118,7 +118,7 @@ choices.forEach(choice => {
         setTimeout(()=>{
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        }, 1500)
+        }, 800)
     })
 })
 
